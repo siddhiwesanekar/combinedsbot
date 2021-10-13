5db62d8267b76c9772a5a04961f7a0931a091d01
siddhiwesanekar <90743476+siddhiwesanekar@users.noreply.github.com>
2021-10-13 10:23:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -12,6 +12,15 @@ import random
 import pickle
 from console_log import ConsoleLog
 import logging
+import requests
+import ast
+
+
+logger = logging.getLogger('console')
+
+
+
+from flask import Flask, render_template, request
 
 bot_name = "SmartBot"
 with open('teamcenter_data.json', 'r') as f:
@@ -211,3 +220,90 @@ def response(sentence, userID='123', show_details=False):
         else:
             return ("I will transfer your question to support team")
 
+#app
+app = Flask(__name__)
+
+
+app.static_folder = './static'
+app.temp_dict = {}
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/get")
+def get_bot_response():
+    logger.info("Error logged from Python")
+    userText = request.args.get('msg')
+    msg5 = response(userText)
+    #print(type(msg5))
+    temp1 = {}
+    if userText.isnumeric():
+        if int(userText) in app.temp_dict.keys():
+            userText = app.temp_dict[int(userText)]
+            userText = ''.join(userText)
+
+    if 'form' in msg5:
+        import webbrowser
+
+        webbrowser.open("D:/PLM Nordic/UOM_request_form.docx")
+    if '/' in msg5:
+        if 'open' in userText:
+            import webbrowser
+            webbrowser.open(msg5)
+
+            return "File opened successfully!"
+
+            # self.text_widget.insert(END,"\n")
+
+        if 'delete' in userText:
+            import os
+            if os.path.exists(msg5):
+                os.remove(msg5)
+                msg5 = "File Deleted Successfully!"
+                return msg5
+
+            else:
+                msg5 = "File Dose Not Exist!"
+                return msg5
+    if (type(msg5) is list) == True:
+            #print("yes")
+
+            listToStr = ''.join([str(elem) for elem in msg5])
+            #print(type(listToStr))
+            #print(listToStr)
+            #return listToStr
+            res = [''.join(ele) for ele in msg5]
+            return render_template('index.html')
+
+
+
+            temp_num = 0
+            for i in res[1:]:
+                temp_num = temp_num + 1
+                app.temp_dict[temp_num] = []
+
+                app.temp_dict[temp_num].append(str(i))
+
+
+            return  app.temp_dict
+       
+
+
+    
+
+    return str(response(userText))
+conversation=[] # Our all conversation
+
+# threading the recv function
+
+
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run()
